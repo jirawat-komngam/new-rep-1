079f8f78c0480d72ac2fe092dc86e5177de1bf3c
jirawat-komngam <140332589+jirawat-komngam@users.noreply.github.com>
2023-07-23 23:49:01 +0700

    correctcodesize

--- Main.java
+++ Main.java
@@ -5,6 +5,6 @@ public class Main {
 
     public static void main(String[] args) {
         new Program().start();
-        System.out.println("kuy yai");
+        System.out.println("kuy yai mak");
     }
 }
